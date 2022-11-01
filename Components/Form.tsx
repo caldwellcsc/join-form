7c361f318889ba8b889272e7031ebc53ad1e6739
vidurkhanal <vkhanal@caldwell.edu>
2022-11-01 19:06:37 -0400

    Deploy 1

--- Components/Form.tsx
+++ Components/Form.tsx
@@ -1,6 +1,10 @@
+import { doc, setDoc } from "firebase/firestore";
 import { Formik } from "formik";
-import { FC } from "react";
+import { FC, useState } from "react";
 import * as Yup from "yup";
+import { db } from "../firebase/client";
+import { v4 as uuidv4 } from "uuid";
+import Image from "next/image";
 
 type FormValues = {
   fullName: string;
@@ -26,108 +30,130 @@ const FormSchema = Yup.object().shape({
 });
 
 const Form: FC = () => {
-  return (
-    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-lg">
-        <h1 className="max-w-5xl text-6xl font-bold leading-none tracking-tighter md:text-5xl lg:text-7xl lg:max-w-7xl text-transparent bg-clip-text bg-gradient-to-b from-sky-400 to-cyan-300 ">
-          Caldwell Computer Science Club
-        </h1>
-        <Formik
-          initialValues={{ email: "", fullName: "", studentID: "" }}
-          validationSchema={FormSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}>
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form
-              onSubmit={handleSubmit}
-              className="mt-6 mb-0 space-y-4 rounded-lg p-8 shadow-2xl">
-              <p className="text-lg font-medium">Join Today.</p>
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  if (!submitted) {
+    return (
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-lg">
+          <h1 className="max-w-5xl text-6xl font-bold leading-none tracking-tighter md:text-5xl lg:text-5xl lg:max-w-7xl text-transparent bg-clip-text bg-indigo-600 text-center">
+            Caldwell Computer Science Club
+          </h1>
+          <Formik
+            initialValues={{ email: "", fullName: "", studentID: "" }}
+            validationSchema={FormSchema}
+            onSubmit={async (values, { setSubmitting }) => {
+              await setDoc(doc(db, "students", uuidv4()), {
+                ...values,
+              });
+              setSubmitted(true);
+            }}>
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form
+                onSubmit={handleSubmit}
+                className="mt-6 mb-0 space-y-4 rounded-lg p-8 shadow-2xl">
+                <p className="text-lg font-medium">Join Today.</p>
 
-              <div>
-                <label className="text-sm font-medium">Name</label>
+                <div>
+                  <label className="text-sm font-medium">Name</label>
 
-                <div className="relative mt-1">
-                  <input
-                    type="text"
-                    id="fullName"
-                    name="fullName"
-                    className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-                    placeholder="Enter Your full name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.fullName}
-                  />
+                  <div className="relative mt-1">
+                    <input
+                      type="text"
+                      id="fullName"
+                      name="fullName"
+                      className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                      placeholder="Enter Your full name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.fullName}
+                    />
+                  </div>
+                  <span className="text-red-500">
+                    {errors.fullName && touched.fullName && errors.fullName}
+                  </span>
                 </div>
-                <span className="text-red-500">
-                  {errors.fullName && touched.fullName && errors.fullName}
-                </span>
-              </div>
 
-              <div>
-                <label className="text-sm font-medium">Email</label>
+                <div>
+                  <label className="text-sm font-medium">Email</label>
 
-                <div className="relative mt-1">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-                    placeholder="Enter Caldwell Email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                  />
+                  <div className="relative mt-1">
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                      placeholder="Enter Caldwell Email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                    />
+                  </div>
+                  <span className="text-red-500 mt-2">
+                    {errors.email && touched.email && errors.email}
+                  </span>
                 </div>
-                <span className="text-red-500 mt-2">
-                  {errors.email && touched.email && errors.email}
-                </span>
-              </div>
 
-              <div>
-                <label className="text-sm font-medium">Student ID</label>
+                <div>
+                  <label className="text-sm font-medium">Student ID</label>
 
-                <div className="relative mt-1">
-                  <input
-                    type="text"
-                    id="studentID"
-                    name="studentID"
-                    className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-                    placeholder="Enter Your Caldwell Student ID"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.studentID}
-                  />
+                  <div className="relative mt-1">
+                    <input
+                      type="text"
+                      id="studentID"
+                      name="studentID"
+                      className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                      placeholder="Enter Your Caldwell Student ID"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.studentID}
+                    />
+                  </div>
+                  <span className="text-red-500">
+                    {errors.studentID && touched.studentID && errors.studentID}
+                  </span>
                 </div>
-                <span className="text-red-500">
-                  {errors.studentID && touched.studentID && errors.studentID}
-                </span>
-              </div>
 
-              <button
-                type="submit"
-                className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
-                disabled={isSubmitting}>
-                Apply
-              </button>
-            </form>
-          )}
-        </Formik>
+                <button
+                  type="submit"
+                  className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+                  disabled={isSubmitting}>
+                  Apply
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-lg">
+          <form className="mt-6 mb-0 space-y-4 rounded-lg p-8 shadow-2xl">
+            <Image
+              src="/submitImage.jpg"
+              height={500}
+              width={500}
+              alt="Submission Confirmation Image"
+            />
+            <p className="text-lg text-gray-700 text-center">
+              Thank you for your applying !! We will get back to you once we
+              process your request.
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Form;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,16 @@ import Head from "next/head";
 import Form from "../Components/Form";
 
 export default function Home() {
+  // const [students, studentLoading, studentErr] = useCollection(
+  //   collection(db, "students")
+  // );
+
+  // if (!studentLoading && students) {
+  //   students.docs.forEach(student => {
+  //     console.log(student.data());
+  //   });
+  // }
+
   return (
     <div>
       <Head>
@@ -9,7 +19,7 @@ export default function Home() {
         <meta name="description" content="Invitation to join Caldwell CSC" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className="lg:bg-gradient-to-r from-rose-100 to-teal-100 min-h-screen">
+      <div className="bg-gradient-to-r from-rose-100 to-teal-100 min-h-screen">
         <Form />
       </div>
     </div>
