4e02abc64a99b1bace33d6dfb39aac4ca87efc2d
vidurkhanal <vkhanal@caldwell.edu>
2022-11-04 09:54:14 -0400

    chore(pages/index.tsx): meta tags and Open Graph Meta tags added for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,38 @@ const Home: React.FC = () => {
             <Head>
                 <title>Join CaldwellCSC</title>
                 <meta
-                    name="description"
-                    content="Invitation to join Caldwell CSC"
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
                 />
+                <meta charSet="UTF-8" />
+
+                <meta
+                    name="description"
+                    content="We are the Caldwell University's tech related student
+                    organization. We hope to create an engaging and positive space for
+                    University's tech enthusiasts and position Caldwell University
+                    within a global community of student leaders in tech. Join Today."
+                />
+                <meta
+                    property="og:title"
+                    content="Join Caldwell CS Club Today."
+                />
+                <meta
+                    property="og:description"
+                    content="We are the Caldwell University's tech related student
+                    organization. We hope to create an engaging and positive space for
+                    University's tech enthusiasts and position Caldwell University
+                    within a global community of student leaders in tech. Join Today."
+                />
+                <meta
+                    property="og:image"
+                    content="https://hoopdirt.com/wp-content/uploads/2022/10/Untitled-design-2022-09-13T112931.122.jpg"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:locale" content="en_US" />
+
+                <meta name="robots" content="index, follow" />
+
                 <link rel="icon" href="/favicon.png" />
             </Head>
             <div className="bg-gradient-to-r from-rose-100 to-teal-100 min-h-screen">
